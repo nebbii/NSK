838a986e43f210f7f4160df34422466e9cfbc5a2
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-11-06 09:44:59 +0100

    csv form made

--- src/AppBundle/Controller/CsvImportController.php
+++ src/AppBundle/Controller/CsvImportController.php
@@ -3,19 +3,37 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class CsvImportController extends Controller
 {
     /**
      * @Route("/importCsv", name="importCsv")
+     * 
      */
-    public function importCsvAction()
+    public function importCsvAction(Request $request)
     {
+        $form = $this->createFormBuilder()
+                ->add('file', FileType::class, [
+                    'mapped' => false,
+                ])
+                ->add('save', SubmitType::class)
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            echo "Submitted";
+
+        }
+
         return $this->render('AppBundle:CsvImport:import_csv.html.twig', array(
-            // ...
+            'form' => $form->createView(),
         ));
     }
 
