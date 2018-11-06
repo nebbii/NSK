272697402923a1e9ab38d305c69c1dba381ee45d
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-11-06 12:04:22 +0100

    able to read csv data!

--- src/AppBundle/Controller/CsvImportController.php
+++ src/AppBundle/Controller/CsvImportController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -29,6 +30,17 @@ class CsvImportController extends Controller
 
         if($form->isSubmitted() && $form->isValid()) {
             echo "Submitted";
+            // get contents from csv file
+            $csv = $form['file']->getData();
+
+            $csvData = file_get_contents($csv->getPathName());
+            //echo "<pre>"; print_r($csv); echo "</pre>";
+            
+            // store into object
+
+            // show form for which columns insert into where
+
+            // make new purchase order & store new items 
 
         }
 
